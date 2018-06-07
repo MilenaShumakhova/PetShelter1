701d7199629ec6282f2d0c2984877d8d28280d87
MilenaShumakhova <chuma1999@mail.ru>
2018-06-08 01:03:34 +0300

    TryToMakeAPanel

--- Team/MyProfile.xaml.cs
+++ Team/MyProfile.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+
+
 namespace Team
 {
     /// <summary>
@@ -26,6 +28,7 @@ namespace Team
 
         private void Menu_Click(object sender, RoutedEventArgs e)
         {
+           
 
         }
     }
