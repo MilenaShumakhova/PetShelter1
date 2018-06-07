aaf1bf3e14ffafefc6f10e049988cef5a9fcfa05
MilenaShumakhova <chuma1999@mail.ru>
2018-06-08 01:07:17 +0300

    New window

--- PetShelterClasses/Model/User.cs
+++ PetShelterClasses/Model/User.cs
@@ -1,6 +1,7 @@
 ﻿using PetShelterClasses.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +15,11 @@ namespace PetShelterClasses
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public Status StatusID { get; set; }
         public string City { get; set; }
         //public List<UserPets> Pets { get; set; }
         public List<Pet> Pet { get; set; }
         public decimal Payment { get; set; }
+
 
 
 
