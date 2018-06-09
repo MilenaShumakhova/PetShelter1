ee7d06126e5049d6c753b1b16510e4be58d3a488
PolinaMilena <pbivakhnik@edu.hse.ru>
2018-06-09 11:51:51 +0300

    AddPetsInDB

--- PetShelterClasses/Migrations/Configuration.cs
+++ PetShelterClasses/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace PetShelterClasses.Migrations
 {
+    using PetShelterClasses.Model;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -18,6 +19,24 @@ namespace PetShelterClasses.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            var PetCt = new Pet { Type = "Cat" };
+            var PetDg = new Pet { Type = "Dog" };
+            var PetFh = new Pet { Type = "Fish" };
+            var PetBd = new Pet { Type = "Bird" };
+            var PetGg = new Pet { Type = "Guinea pig" };
+            var PetMe = new Pet { Type = "Home mouse" };
+            var PetRt = new Pet { Type = "Rabbit" };
+            var PetDt = new Pet { Type = "Decorative rat" };
+            var PetUn = new Pet { Type = "Pygmy hedgehog" };
+            var PetHr = new Pet { Type = "Hamster" };
+            var PetSl = new Pet { Type = "Snail" };
+            var PetFx = new Pet { Type = "Fox" };
+            var PetIt = new Pet { Type = "Insect" };
+            var PetSe = new Pet { Type = "Snake" };
+            context.Pets.AddOrUpdate(p => p.Type, PetBd, PetCt, PetDg, PetDt, PetFh, PetFx, PetGg, PetHr, PetIt, PetMe, PetRt, PetSe,PetSl,PetUn);
+            context.SaveChanges();
+
         }
     }
 }
