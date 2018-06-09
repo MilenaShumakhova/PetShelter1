8c57e6f7b2cb911b0bb4a3fd35d806a1b5ddf3ce
MilenaShumakhova <chuma1999@mail.ru>
2018-06-09 12:07:18 +0300

    HamburgerMenu

--- Team/MyProfile.xaml.cs
+++ Team/MyProfile.xaml.cs
@@ -9,9 +9,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Team.Pages;
 
 namespace Team
 {
@@ -20,10 +22,12 @@ namespace Team
     /// </summary>
     public partial class MyProfile : Window
     {
+        bool StateClosed = true;
         public MyProfile()
         {
             InitializeComponent();
-            
+   
+         
         }
 
         private void Menu_Click(object sender, RoutedEventArgs e)
@@ -36,6 +40,39 @@ namespace Team
             int val = Convert.ToInt32(e.NewValue);
             string msg = String.Format("Current value: {0}", val);
             this.textBlock1.Text = msg;
+        }
+
+        private void ButtonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (StateClosed)
+            {
+                Storyboard sb = this.FindResource("OpenMenu") as Storyboard;
+                sb.Begin();
+            }
+            else
+            {
+                Storyboard sb = this.FindResource("CloseMenu") as Storyboard;
+                sb.Begin();
+            }
+
+            StateClosed = !StateClosed;
+        }
+
+        private void RadioButton_ClickRequests(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_ClickGiver(object sender, RoutedEventArgs e)
+        {
+            //Page1 page = new Page1();
+            //ContentFrame.Navigate(page);
+            ////NavigationService nav = NavigationService.GetNavigationService(this);
+            ////nav.Navigate(new Uri("/Page1.xaml", UriKind.Relative));
+            //ContentFrame.Navigate(new System.Uri("/Pages/Page1.xaml",
+            // UriKind.RelativeOrAbsolute));
+            ContentFrame.Navigate( typeof(Page1));
+           
         }
     }
 }
